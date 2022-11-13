493d0fd504b9386b154e198253d008ce3551b78c
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2022-11-14 01:07:15 +0300

    feat: handling multiple connections

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 use std::io::BufRead;
 use std::io::{BufReader, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_connection(mut _stream: TcpStream) {
+    let mut buf = BufReader::new(_stream.try_clone().expect("stream clone failed"));
+    loop {
+        let mut line = String::new();
+        match buf.read_line(&mut line) {
+            Ok(size) => {
+                println!("Received data {} bytes {}", size, line);
+                if line.contains("ping") {
+                    _stream.write("+PONG\r\n".as_bytes()).unwrap();
+                }
+            }
+            Err(e) => println!("error while receiving data: {}", e),
+        }
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -9,19 +26,9 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                let mut buf = BufReader::new(_stream.try_clone().unwrap());
-                loop {
-                    let mut line = String::new();
-                    match buf.read_line(&mut line) {
-                        Ok(size) => {
-                            println!("Received data {} bytes {}", size, line);
-                            if line.contains("ping") {
-                                _stream.write("+PONG\r\n".as_bytes()).unwrap();
-                            }
-                        }
-                        Err(e) => println!("error while receiving data: {}", e),
-                    }
-                }
+                thread::spawn(move || {
+                    handle_connection(_stream.try_clone().expect("stream clone failed"));
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
