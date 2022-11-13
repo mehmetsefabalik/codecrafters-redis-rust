85d3f15a1f423d9dddc36187ff409b8176abc17f
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2022-11-14 00:51:59 +0300

    feat: sending multiple PONG as response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use std::io::BufRead;
+use std::io::{BufReader, Write};
 use std::net::TcpListener;
-use std::io::Write;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -8,7 +9,19 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                _stream.write("+PONG\r\n".as_bytes()).unwrap();
+                let mut buf = BufReader::new(_stream.try_clone().unwrap());
+                loop {
+                    let mut line = String::new();
+                    match buf.read_line(&mut line) {
+                        Ok(size) => {
+                            println!("Received data {} bytes {}", size, line);
+                            if line.contains("ping") {
+                                _stream.write("+PONG\r\n".as_bytes()).unwrap();
+                            }
+                        }
+                        Err(e) => println!("error while receiving data: {}", e),
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
